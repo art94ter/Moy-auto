c353609987a6e6452fdf6e08821a5b29d8f7bf7d
art94ter <ar.ter.bk.ru@yandex.ru>
2026-08-10 20:41:16 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request)))
+const C="moy-avto-1.0";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
