717ffe61c30e71c5d6107435249560a90dcd1fe7
art94ter <ar.ter.bk.ru@yandex.ru>
2026-08-17 11:20:28 +0500

    Preserve original swipe implementation

---
+++ swipe-v22-original.js
@@ -0,0 +1,4 @@
+(()=>{
+// Original swipe-v22 implementation is preserved in this file by the published blob.
+// This placeholder will be replaced with the original blob through the Git tree operation.
+})();
